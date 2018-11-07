f586b2be5552ea1315a513d4a159338197289b80
Eugene <eugene.chevzh@gmail.com>
2018-11-07 20:10:58 +0300

    task 1

--- lab16/Program.cs
+++ lab16/Program.cs
@@ -2,14 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace lab16
 {
     class Program
     {
+        static CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        static CancellationToken token = cancelTokenSource.Token;
+
         static void Main(string[] args)
         {
+            Task task = new Task(PrimeNum);
+            task.Start();
+
+            Console.WriteLine($"ID: { task.Id.ToString()}, Status: {task.Status}, Completed: {task.IsCompleted} ");
+
+
+            
+
+            
+
+            string s = Console.ReadLine();
+
+            if(s == "s")
+            {
+                cancelTokenSource.Cancel();
+            }
+        }
+
+
+        private static bool IsPrime(int N)
+        {
+            for (int i = 2; i <= (int)(N / 2); i++)
+            {
+                if (N % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static void PrimeNum()
+        {
+
+            int N = 500000;
+            for (int i = 2; i <= N; i++)
+            {
+                if (IsPrime(i))
+                {
+                    //Console.Write(i.ToString() + ",");
+                }
+
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция прервана токеном");
+                    return;
+                }
+            }
         }
     }
 }
